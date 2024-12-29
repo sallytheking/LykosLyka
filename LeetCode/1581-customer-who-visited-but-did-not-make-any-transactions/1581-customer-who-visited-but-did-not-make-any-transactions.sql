5e4f6fef0e55c226ec08b9cd24bb355349ed9775
sallytheking <150634339+sallytheking@users.noreply.github.com>
2024-12-29 15:24:12 +0900

    Time: 1873 ms (28.98%), Space: 0B (100%) - LeetHub

--- LeetCode/1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ LeetCode/1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -2,8 +2,6 @@
 SELECT A.customer_id
      , count(A.visit_id) AS count_no_trans
   FROM Visits A 
---   LEFT
---   JOIN Transactions B ON A.visit_id = B.visit_id
  WHERE A.visit_id NOT IN (
     SELECT visit_id
       FROM Transactions)
