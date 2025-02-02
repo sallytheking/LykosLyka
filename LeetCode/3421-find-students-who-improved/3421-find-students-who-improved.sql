8399e73b7dbc3bf17dd2c646b65ad6a8c2bcf734
sallytheking <150634339+sallytheking@users.noreply.github.com>
2025-02-02 15:12:44 +0900

    Time: 313 ms (94.81%), Space: 0B (100%) - LeetHub

--- LeetCode/3421-find-students-who-improved/3421-find-students-who-improved.sql
+++ LeetCode/3421-find-students-who-improved/3421-find-students-who-improved.sql
@@ -1,28 +1,13 @@
-WITH first_exam AS (
-    -- 학생별, 과목별 가장 첫 번째 시험 점수를 찾음
-    SELECT student_id, subject, score AS first_score
-    FROM Scores
-    WHERE (student_id, subject, exam_date) IN (
-        SELECT student_id, subject, MIN(exam_date)
-        FROM Scores
-        GROUP BY student_id, subject
-    )
-),
-latest_exam AS (
-    -- 학생별, 과목별 가장 최근 시험 점수를 찾음
-    SELECT student_id, subject, score AS latest_score
-    FROM Scores
-    WHERE (student_id, subject, exam_date) IN (
-        SELECT student_id, subject, MAX(exam_date)
-        FROM Scores
-        GROUP BY student_id, subject
-    )
+# first_score, latest_score
+WITH improvement AS
+(
+    SELECT student_id, subject, score
+         , FIRST_VALUE(score) OVER (PARTITION BY subject, student_id ORDER BY exam_date ASC) AS first_score
+         , FIRST_VALUE(score) OVER (PARTITION BY subject, student_id ORDER BY exam_date DESC) AS latest_score 
+      FROM Scores
 )
 
-SELECT f.student_id, f.subject, f.first_score, l.latest_score
-FROM first_exam f
-JOIN latest_exam l 
-ON f.student_id = l.student_id 
-AND f.subject = l.subject
-WHERE l.latest_score > f.first_score
-ORDER BY f.student_id, f.subject;
+SELECT DISTINCT student_id, subject, first_score, latest_score
+  FROM improvement
+ WHERE latest_score > first_score
+ ORDER BY student_id, subject;
